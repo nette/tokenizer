c493952bcecf1b1ae1782cd40f723363d2f3a7e9
David Grudl <david@grudl.com>
2015-10-05 14:26:59 +0200

    used https

--- src/Tokenizer/TokenizerException.php
+++ src/Tokenizer/TokenizerException.php
@@ -1,12 +1,8 @@
 <?php
 
 /**
- * This file is part of the Nette Framework (http://nette.org)
- *
- * Copyright (c) 2004, 2011 David Grudl (http://davidgrudl.com)
- *
- * For the full copyright and license information, please view
- * the file license.txt that was distributed with this source code.
+ * This file is part of the Nette Framework (https://nette.org)
+ * Copyright (c) 2004 David Grudl (https://davidgrudl.com)
  */
 
 namespace Nette\Utils;
